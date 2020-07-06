ae8c19a1e3b76e692edb5716249f902672e8115e
Adebayo Adesokan <60199973+B-you@users.noreply.github.com>
2020-07-06 16:07:18 +0300

    Update constants.js

--- react/features/base/settings/constants.js
+++ react/features/base/settings/constants.js
@@ -3,4 +3,4 @@
 /**
  * The default server URL to open if no other was specified.
  */
-export const DEFAULT_SERVER_URL = 'https://jm01.multipliersolutions.in';
+export const DEFAULT_SERVER_URL = 'https://vcinvite.tilda.com.tr';
