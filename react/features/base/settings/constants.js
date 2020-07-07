ddd0d6729058140d0e2116ec2f1e228a41578d26
Adebayo Adesokan <60199973+B-you@users.noreply.github.com>
2020-07-07 12:05:47 +0300

    Update constants.js

--- react/features/base/settings/constants.js
+++ react/features/base/settings/constants.js
@@ -3,4 +3,4 @@
 /**
  * The default server URL to open if no other was specified.
  */
-export const DEFAULT_SERVER_URL = 'https://vc.tilda.com.tr';
+export const DEFAULT_SERVER_URL = 'https://meeting.vclink.world:8000/';
